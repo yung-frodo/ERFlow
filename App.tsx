ac548f2ec4b34a6d8c57d573c72fc870ae6e3cf1
yung-frodo <40253658+yung-frodo@users.noreply.github.com>
2019-10-28 16:01:53 -0400

    Give functionality to text input and button for MRN view, add views
    adds 3 views to enum for 3 different views in app, then adds functionality for MRN view to show up initially and logging view to show up after button is pressed

--- App.tsx
+++ App.tsx
@@ -1,22 +1,45 @@
 import React from 'react';
 import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
+enum ERFlowView {
+    LOGIN = 0,
+    MRN = 1,
+    LOGGING = 2,
+};
+
 export default function App() {
-    return (
-          <View style={styles.container}>
-              <Text>Enter MRN</Text>
-              <TextInput
-                  style={{height: 40}}
-                  placeholder="Type here for MRN!"
-                  // onChangeText={(text) => this.setState({text})}
-                  value=''
-              />
-              <Button
-                  title="Go!"
-                  // onPress =
-              />
-          </View>
-      );
+    const [textValue, onChangeText] = React.useState('');
+    const [currView, onButtonPress] = React.useState(ERFlowView.MRN);
+
+    const MRNview =
+      <View style={styles.container}>
+          <Text>Enter MRN</Text>
+          <TextInput
+              style={{height: 40, borderColor: 'gray', borderWidth: 1}}
+              placeholder="Type here for MRN!"
+              onChangeText={(newText) => onChangeText(newText)}
+              value={textValue}
+          />
+          <Button
+              title="Go!"
+              onPress={() => {
+                onButtonPress(ERFlowView.LOGGING);
+              }}
+          />
+      </View>;
+
+    const defaultView =
+        <View style={styles.container}>
+            <Text>{currView}</Text>
+            <Text>{textValue}</Text>
+        </View>;
+
+    switch(currView) {
+        case ERFlowView.MRN:
+            return MRNview;
+        default:
+            return defaultView;
+    }
 }
 
 const styles = StyleSheet.create({
